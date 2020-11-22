e4479bd11294f6a3c654c02f10a2ae5a4d6b1854
Gefgs <62507867+Gefgs@users.noreply.github.com>
2020-11-22 11:32:04 +0300

    fiat: add all important type

---
+++ src/Howework 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Howework_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 1234.12M;
+            var b = 0.0031M;
+            Console.WriteLine(a + b);
+            double x = 0.123;
+            double y = 1234.0000011;
+            Console.WriteLine(x + y);
+            Console.WriteLine(a);
+            Console.WriteLine(x);
+            int z = 1;
+            Console.WriteLine(z.GetHashCode());
+            string aa = "12_3+";
+            Console.WriteLine(aa.TrimStart('1','2','3','4').TrimEnd('+'));
+            var ll = 'k';
+            Console.WriteLine(ll.Equals('1'));
+
+        }
+    }
+}
